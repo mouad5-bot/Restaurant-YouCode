c8f723f3a25ea7709e6cc030325fda0ed74e9934
MOUAD FIFEL <mouad.fifel5@gmail.com>
2023-02-05 23:56:47 +0100

    RYC-4  adding postController  | its route also

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
 Route::get('users/profile', function () {
     return view('users/profile');
 })->name('users/profile');
+
+// _______________________________________________________
+
+Route::get('/posts/{post}', function ( $id ) {
+    $post = Post::findOrFail($id);
+});
